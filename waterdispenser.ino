0b13d586186d4a9fb44a0d57b9a3b071e0a2792c
gaugfather <derek.gauger@gmail.com>
2021-04-24 00:26:28 -0500

    auto button

--- waterdispenser.ino
+++ waterdispenser.ino
@@ -2,6 +2,7 @@
 
 const int FILL_BUTTON = 22;
 const int STOP_BUTTON = 24;
+const int AUTO_BUTTON = 26;
 
 boolean fill_lastButton = LOW;
 boolean fill_currentButton = LOW;
@@ -10,6 +11,12 @@ boolean is_filling = false;
 boolean stop_lastButton = LOW;
 boolean stop_currentButton = LOW;
 boolean is_stopped = true;
+
+boolean auto_lastButton = LOW;
+boolean auto_currentButton = LOW;
+boolean auto_filling = false;
+unsigned long auto_startTime = 0;
+unsigned long AUTO_FILL_TIME = 10000; // 10 sec
 
 void setup() {
    Serial.begin(9600);
@@ -33,21 +40,40 @@ void monitorFillButton() {
   }
   fill_lastButton = fill_currentButton;
   while(is_filling && !is_stopped) {
-    Serial.println("FILLING...");
+    Serial.println("MANUAL-FILLING...");
     monitorStopButton();
   }
 }
 
 void monitorStopButton() {
   stop_currentButton = debounceButton(stop_lastButton, STOP_BUTTON);
-  if(stop_lastButton == LOW && stop_currentButton == HIGH && is_filling) {
-    Serial.println("STOPPED!!");
+  if(stop_lastButton == LOW && stop_currentButton == HIGH && (is_filling || auto_filling)) {
+    Serial.println("STOPPED FILLING!!");
     is_stopped = true;
     is_filling = false;
+    auto_filling = false;
   }
   stop_lastButton = stop_currentButton;
 }
 
+void monitorAutoButton() {
+  auto_currentButton = debounceButton(auto_lastButton, AUTO_BUTTON);
+  if(auto_lastButton == LOW && auto_currentButton == HIGH) { 
+    is_stopped = false;
+    auto_filling = true;
+    auto_startTime = millis();
+  }
+  auto_lastButton = auto_currentButton;
+  while(auto_filling && !is_stopped && ((millis() - auto_startTime) <= AUTO_FILL_TIME)) {
+    Serial.println("AUTO-FILLING (" + String((millis() - auto_startTime) / 1000) + " of 10 seconds)");
+    monitorStopButton(); 
+  }
+  auto_filling = false;
+  is_stopped = true;
+  auto_startTime = 0; 
+}
+
 void loop() {
   monitorFillButton();
+  monitorAutoButton();
 }
