0a455d771f249e6607dfc58ed3697af868aff964
gaugfather <derek.gauger@gmail.com>
2021-05-28 13:05:57 -0500

    refactored

--- waterdispenser.ino
+++ waterdispenser.ino
@@ -5,37 +5,70 @@ LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7);
 
 const boolean SERIAL_DEBUG = false;
 
+// ************************
+// * PINS
+// ************************
 const int FILL_BUTTON = 22;
 const int STOP_BUTTON = 24;
 const int ON_BUTTON = 26;
-
 const int SOLENOID = 35;
 
-boolean fill_lastButton = LOW;
-boolean fill_currentButton = LOW;
-boolean is_filling = false;
 
-boolean stop_lastButton = LOW;
-boolean stop_currentButton = LOW;
-boolean is_stopped = true;
-
+// ************************
+// * BUTTON - ON - (status)
+// ************************
 boolean on_lastButton = LOW;
 boolean on_currentButton = LOW;
 boolean is_on = false;
 
-unsigned long fill_startTime = 0;
+unsigned long DISPLAY_TIMER = 60000; // 1 min
+unsigned long display_startTime = 0;
+unsigned long display_endTime = 0;
+
+
+// ************************
+// * BUTTON - FILL - (status)
+// ************************
+boolean fill_lastButton = LOW;
+boolean fill_currentButton = LOW;
+boolean is_filling = false;
+
 unsigned long AUTO_FILL_TIME = 152000; // 2 min 32 sec
+unsigned long fill_startTime = 0;
 
-unsigned long TIMER = 60000; // 1 min
-unsigned long startTime = 0;
-unsigned long endTime = 0;
+int droplet_col_1_animation_row = 0;
+int droplet_col_2_animation_row = 2;
 
-int water_animation2 = 1;
-int water_animation2_pic = 0;
-int water_animation3 = 2;
-int water_animation3_pic = 0;
+int droplet_col_1_animation_picture = 0;
+int droplet_col_2_animation_picture = 0;
 
-byte water[8] = {
+ 
+// ************************
+// * BUTTON - STOP - (status)
+// ************************
+boolean stop_lastButton = LOW;
+boolean stop_currentButton = LOW;
+boolean is_stopped = true;
+
+
+// ************************
+// * serial log messages if debug is enabled
+// ************************
+void serialDebug(String message){
+  if(SERIAL_DEBUG){
+    Serial.println(message);
+  }
+}
+
+
+// ************************
+// * Setup LCD display and emojis
+// ************************
+void setupLcdDisplay() {
+  lcd.begin(20,4);
+  lcd.setBacklightPin(3, POSITIVE);
+
+  byte water_droplet[8] = {
       0b00000,
       0b00100,
       0b01110,
@@ -44,8 +77,10 @@ byte water[8] = {
       0b11111,
       0b01110,
       0b00000
-};
-byte waterEnd[8] = {
+  };
+  lcd.createChar(0, water_droplet);
+  
+  byte water_droplet_splash[8] = {
       0b00100,
       0b00000,
       0b10001,
@@ -54,8 +89,10 @@ byte waterEnd[8] = {
       0b00100,
       0b01110,
       0b11111
-};  
-byte skull[8] = {
+  };
+  lcd.createChar(1, water_droplet_splash);
+
+  byte skull[8] = {
       0b00000,
       0b01110,
       0b10101,
@@ -64,8 +101,10 @@ byte skull[8] = {
       0b01110,
       0b00000,
       0b00000
-};
-byte alien[8] = {
+  };
+  lcd.createChar(2, skull);
+  
+  byte alien[8] = {
       0b11111,
       0b10101,
       0b11111,
@@ -74,30 +113,17 @@ byte alien[8] = {
       0b01010,
       0b11011,
       0b00000
-};
-
-void serialDebug(String message){
-  if(SERIAL_DEBUG){
-    Serial.println(message);
-  }
-}
-void setup() {
-  if(SERIAL_DEBUG) {
-   Serial.begin(9600); 
-  }
-  
-  pinMode(SOLENOID, OUTPUT);
-
-  lcd.begin(20,4);
-  lcd.setBacklightPin(3,POSITIVE);
-  setDisplayHeader();
-  lcd.createChar(0, water);
-  lcd.createChar(1, waterEnd);
-  lcd.createChar(2, skull);
+  };
   lcd.createChar(3, alien);
+
+  setLcdDisplayHeader();   
 }
 
-void setDisplayHeader() {
+
+// ************************
+// * LCD display Header
+// ************************
+void setLcdDisplayHeader() {
   lcd.clear();
   lcd.setCursor( 1, 0);
   lcd.write(byte(3));
@@ -107,138 +133,126 @@ void setDisplayHeader() {
   lcd.setCursor(6,1);
 }
 
-void clearLcdPoint(int row, int column) {
-  serialDebug("Clear row:" + String(row) + ", column: " + String(column));
-  lcd.setCursor(column, row);
-  lcd.write(" ");
-}
 
-void clearAnimation(int row, int column) {
-  if(row != 0) {
-    clearLcdPoint(0, column);
-  }
-  if(row != 1) {
-    clearLcdPoint(1, column);
-  }
-  if(row != 2) {
-    clearLcdPoint(2, column);
-  }
-  if(row != 3) {
-    clearLcdPoint(3, column);
-  }
-  
-}
-
-void waterAnimation() {
-  water_animation2 = water_animation2 + 1;
-  water_animation3 = water_animation3 + 1;
-  if(water_animation2 > 4) {
-    water_animation2 = 0;
-  } 
-  if(water_animation2 == 3) {
-    water_animation2_pic = 1;
-  } else {
-    water_animation2_pic = 0;
-  }
-  if(water_animation3 > 4) {
-    water_animation3 = 0;
-  }
-  if(water_animation3 == 3) {
-    water_animation3_pic = 1;
-  } else {
-    water_animation3_pic = 0;
-  }
-  clearAnimation(water_animation2, 18);
-  clearAnimation(water_animation3, 19);
- 
-  lcd.setCursor(18, water_animation2);
-  lcd.write(byte(water_animation2_pic));
-  lcd.setCursor(19, water_animation3);
-  lcd.write(byte(water_animation3_pic));
-}
-void setDisplayFilling() {
-  lcd.setCursor(3,1);
-}
-
-boolean toggleDisplayOff() {
-  is_on = false;
-  lcd.setBacklight(LOW);
-  startTime = 0;
-  endTime = 0;
-  setDisplayHeader();
-}
-
-boolean toggleDisplayOn() {
+// ************************
+// * Turns on (brightens) LCD Display
+// ************************
+boolean turnLcdDisplayOn() {
   is_on = true;
   lcd.setBacklight(HIGH);
-  startTime = millis();
-  endTime = millis() + TIMER;
+  display_startTime = millis();
+  display_endTime = millis() + DISPLAY_TIMER;
 }
 
+
+// ************************
+// * Turns off (dims) LCD Display
+// ************************
+boolean turnLcdDisplayOff() {
+  is_on = false;
+  lcd.setBacklight(LOW);
+  display_startTime = 0;
+  display_endTime = 0;
+  setLcdDisplayHeader();
+}
+
+
+// ************************
+// * Debounce button reads
+// ************************
 boolean debounceButton(boolean last, int button) {
   boolean current = digitalRead(button);
   if(last != current) {
-    delay(5); //5ms
+    delay(5); // 5 ms
     current = digitalRead(button);
   }
   return current;
+}
+
+
+// ************************
+// * LCD display Filling
+// ************************
+void setLcdDisplayFilling(int percent) {
+  lcd.setCursor(0,2);
+  lcd.print("drinking gaug-ter");
+  lcd.setCursor(2,3);
+  lcd.write(byte(0));
+  lcd.write(byte(0));
+  lcd.write(byte(0));
+  lcd.print(" |" + String(percent) + "%" + "| ");
+  lcd.write(byte(0));
+  lcd.write(byte(0));
+  lcd.write(byte(0));
+
+  droplet_col_1_animation_row = lcdDisplayFilling_getWaterAnimationRow(droplet_col_1_animation_row);
+  droplet_col_1_animation_picture = lcdDisplayFilling_getWaterAnimationPicture(droplet_col_1_animation_row);
+  lcdDisplayFilling_clearAnimation(18, droplet_col_1_animation_row);
+  lcdDisplayFilling_printAnimation(18, droplet_col_1_animation_row, droplet_col_1_animation_picture);
+  
+  droplet_col_2_animation_row = lcdDisplayFilling_getWaterAnimationRow(droplet_col_2_animation_row);
+  droplet_col_2_animation_picture = lcdDisplayFilling_getWaterAnimationPicture(droplet_col_2_animation_row);
+  lcdDisplayFilling_clearAnimation(19, droplet_col_2_animation_row);
+  lcdDisplayFilling_printAnimation(19, droplet_col_2_animation_row, droplet_col_2_animation_picture);
+}
+
+int lcdDisplayFilling_getWaterAnimationRow(int dropletRow) {
+  dropletRow = dropletRow + 1;
+  if(dropletRow == 4) {
+    dropletRow = 0;
+  }
+  return dropletRow;
+}
+
+int lcdDisplayFilling_getWaterAnimationPicture(int dropletRow) {
+  int dropletPicture = 0;
+  if(dropletRow == 3) {
+    dropletPicture = 1;
+  }
+  return dropletPicture;
+}
+
+void lcdDisplayFilling_clearAnimation(int column, int row) {
+  if(row != 0) {
+    lcdDisplayFilling_clearLcdPoint(0, column);
+  }
+  if(row != 1) {
+    lcdDisplayFilling_clearLcdPoint(1, column);
+  }
+  if(row != 2) {
+    lcdDisplayFilling_clearLcdPoint(2, column);
+  }
+  if(row != 3) {
+    lcdDisplayFilling_clearLcdPoint(3, column);
+  }
   
 }
 
-void triggerSolenoid() {
-  serialDebug("HIGH SOLENOID");
-  digitalWrite(SOLENOID, HIGH);
-  delay(10000); //10000 is 10 seconds
-  serialDebug("LOW SOLENOID");
-  digitalWrite(SOLENOID, LOW);
-  delay(10000);
-}
+void lcdDisplayFilling_clearLcdPoint(int row, int column) {
+  serialDebug("Clear row:" + String(row) + ", column: " + String(column));  
+  lcd.setCursor(column, row);
 
-void fillWater() {
-  serialDebug("FILL WATER...");
-  setDisplayFilling();
-  digitalWrite(SOLENOID, HIGH);
-}
-
-void stopFillWater() {
-  serialDebug("STOP FILL WATER...");
-
-  clearAnimation(4, 18);
-  clearAnimation(4, 19);
-
-  lcd.setCursor(0,2);
-  lcd.print("    stopped!     ");  
-  lcd.setCursor(0,3);
-  lcd.print("      |");
-  lcd.write(byte(2));
-  lcd.write(byte(2));
-  lcd.print("|     ");
-  endTime = millis() + TIMER;
-  digitalWrite(SOLENOID, LOW);
-}
-
-void monitorButtons() {
-  on_currentButton = debounceButton(on_lastButton, ON_BUTTON);
-  if(!is_on && on_currentButton){
-    toggleDisplayOn();
-    on_lastButton = LOW;
+  unsigned long startTime = millis();
+  unsigned long endTime = startTime;
+  
+  // delay between water droplet animation 20ms
+  while ((endTime - startTime) <=20) { 
+    monitorStopButton();
+    endTime = millis();
   }
-  if(is_on) {
-    monitorFillButton();
-  }
+  
+  lcd.write(" ");
 }
 
-void monitorStopButton() {
-  stop_currentButton = debounceButton(stop_lastButton, STOP_BUTTON);
-  if(stop_lastButton == LOW && stop_currentButton == HIGH && (is_filling)) {
-    serialDebug("STOPPED FILLING!!");
-    is_stopped = true;
-    is_filling = false;
-    stopFillWater();
-  }
-  stop_lastButton = stop_currentButton;
+void lcdDisplayFilling_printAnimation(int column, int row, int pic) {
+  lcd.setCursor(column, row);
+  lcd.write(byte(pic));
 }
 
+
+// ************************
+// * Monitors Fill Button Press
+// ************************
 void monitorFillButton() {
   fill_currentButton = debounceButton(fill_lastButton, FILL_BUTTON);
 
@@ -246,12 +260,13 @@ void monitorFillButton() {
     is_stopped = false;
     is_filling = true;
     fill_startTime = millis();
-    fillWater();
+
+    serialDebug("FILLING WATER...");
+    digitalWrite(SOLENOID, HIGH);
   }
   while(is_filling && !is_stopped && ((millis() - fill_startTime) <= AUTO_FILL_TIME)) {
     serialDebug("AUTO-FILLING (" + String((millis() - fill_startTime) / 1000) + " seconds of " + String((AUTO_FILL_TIME / 1000)) + " seconds)");
-    lcd.setCursor(2,1);
-   
+    
     double startTime = (millis() - fill_startTime) / 1000 / (AUTO_FILL_TIME / 1000);
     double num1 = (millis() - fill_startTime) / 1000;
     double num2 = AUTO_FILL_TIME / 1000;
@@ -259,24 +274,16 @@ void monitorFillButton() {
     int fillPercent = (millis() - fill_startTime) / 1000 / ( AUTO_FILL_TIME / 1000);
     int percent = round(num3);
     serialDebug("FILL PERCENT = " + String(fillPercent) + "%");
-    lcd.setCursor(0,2);
-    lcd.print("drinking gaug-ter");
-    lcd.setCursor(2,3);
-    lcd.write(byte(0));
-    lcd.write(byte(0));
-    lcd.write(byte(0));
-    lcd.print(" |" + String(percent) + "%" + "| ");
-    lcd.write(byte(0));
-    lcd.write(byte(0));
-    lcd.write(byte(0));
-    waterAnimation();
+    
+    setLcdDisplayFilling(percent);
     monitorStopButton(); 
   }
   if(fill_lastButton == LOW && fill_currentButton == HIGH) {
-    stopFillWater();
+    setLcdDisplayStopped();
+    digitalWrite(SOLENOID, LOW);
   }
-  if(is_on && is_stopped && (millis() > endTime)) {
-    toggleDisplayOff();
+  if(is_on && is_stopped && (millis() > display_endTime)) {
+    turnLcdDisplayOff();
   }
   
   fill_lastButton = fill_currentButton; 
@@ -285,6 +292,73 @@ void monitorFillButton() {
   fill_startTime = 0; 
 }
 
+
+// ************************
+// * LCD Display Stopped
+// ************************
+void setLcdDisplayStopped() {
+  serialDebug("STOPPED FILLING WATER...");
+
+  lcdDisplayFilling_clearAnimation(18, 4);
+  lcdDisplayFilling_clearAnimation(19, 4);
+
+  lcd.setCursor(0,2);
+  lcd.print("    stopped!     ");  
+  lcd.setCursor(0,3);
+  lcd.print("      |");
+  lcd.write(byte(2));
+  lcd.write(byte(2));
+  lcd.print("|     ");
+  display_endTime = millis() + DISPLAY_TIMER;
+}
+
+
+// ************************
+// * Monitors Stop Button Press
+// ************************
+void monitorStopButton() {
+  stop_currentButton = debounceButton(stop_lastButton, STOP_BUTTON);
+  if(stop_lastButton == LOW && stop_currentButton == HIGH && (is_filling)) {
+    serialDebug("STOPPED FILLING!!");
+    is_stopped = true;
+    is_filling = false;
+    setLcdDisplayStopped();
+    digitalWrite(SOLENOID, LOW);
+  }
+  stop_lastButton = stop_currentButton;
+}
+
+
+// ************************
+// * Monitor Button Events
+// ************************
+void monitorButtons() {
+  on_currentButton = debounceButton(on_lastButton, ON_BUTTON);
+  if(!is_on && on_currentButton){
+    turnLcdDisplayOn();
+    on_lastButton = LOW;
+  }
+  if(is_on) {
+    monitorFillButton();
+  }
+}
+
+
+// ************************
+// * Arduino MAIN SETUP
+// ************************
+void setup() {
+  if(SERIAL_DEBUG) {
+   Serial.begin(9600); 
+  }
+  pinMode(SOLENOID, OUTPUT);
+  setupLcdDisplay();
+}
+
+
+// ************************
+// * Arduino MAIN LOOP
+// ************************
 void loop() {
   monitorButtons();
 }
